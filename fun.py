e3a1571bc1be937ee33ec54dee34bca43006b71d
b0dmas <wsuits6@gmail.com>
2025-06-04 14:52:10 +0000

    Added EMotions to Fun.py

--- fun.py
+++ fun.py
@@ -1,36 +1,87 @@
-#let shave fun
 
-print("lets play with the Print staement")
-#creating a frown and smile fuction
-def smile():
-    print("       +          +       ")
-    print("     + + +      + + +     ")
-    print("       +          +       ")
-    print("                          ")
-    print("        +++     +++       ")
-    print("         +       +        ")
-    print("          + + + +         ")
-    print("            +++           ")
+# Let's have fun
 
-#frown Fuction
+print("Welcome to the Fun Program!")
+print("This program will display a smiley or frown face based on your input.")
 
-def frown():
-    print("       +          +       ")
-    print("     + + +      + + +     ")
-    print("       +          +       ")
-    print("                          ")
-    print("            +++           ")
-    print("         +       +        ")
-    print("        +         +       ")
-    print("       +++       +++      ")
+# Creating a smile function
+def happy():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  o   o  ]   ")
+    print("   |    ^    |   ")
+    print("   |  \\___/  |   ")
+    print("    +_______+    ")
 
-#Asking if the user is happy or not 
-#TO utilize the fronw fuction
-state = input(" [+] Are you Happy: Yes(y) No(n) :::  ")
-if state == "y":
-    smile()
-elif state == "n":
-    frown()
-else:
-    print("SORRY I DONT GET YOU: ")
+# Creating a frown function
+def sad():
+    print("       +\"\"\"\"\"+       ")
+    print("     [  o   o  ]     ")
+    print("     |    ^    |     ")
+    print("     |   ___   |     ")
+    print("     |  /   \\  |     ")
+    print("      +_______+      ")
+    print("     /         \\    ")
+    print("    /           \\   ")
+    print("   +-------------+  ")
+
+def angry():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  >   <  ]   ")
+    print("   |    ~    |   ")
+    print("   |  _____  |   ")
+    print("    +_______+    ")
+
+def surprised():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  O   O  ]   ")
+    print("   |    o    |   ")
+    print("   |   ___   |   ")
+    print("    +_______+    ")
+
+def neutral():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  o   o  ]   ")
+    print("   |    -    |   ")
+    print("   |  _____  |   ")
+    print("    +_______+    ")
+
+def cool():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  B   B  ]   ")
+    print("   |    ^    |   ")
+    print("   |  \\___/  |   ")
+    print("    +_______+    ")
+
+def crying():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  o   o  ]   ")
+    print("   |    ~    |   ")
+    print("   |  \\___/  |   ")
+    print("   |  /   \\  |   ")
+    print("  /         \\  ")
+    print(" /    '''    \\ ")
+    print("+-------------+")
+
+def silly():
+    print("     +\"\"\"\"\"+     ")
+    print("   [  o   o  ]   ")
+    print("   |    ~    |   ")
+    print("   |  \\___/  |   ")
+    print("   |   ---   |   ")
+    print("    +_______+    ")
+
+
+
+# While loop to repeat the process
+# while True:
+#     state = input("\n[+] Are you Happy? Yes (y) / No (n) / Quit (q): ").lower()
     
+#     if state == "y":
+#         happy()
+#     elif state == "n":
+#         sad()
+#     elif state == "q":
+#         print("[-] Exiting the Fun Program. See you next time!")
+#         break
+#     else:
+#         print("[!] Invalid input. Please enter 'y', 'n', or 'q'.")
